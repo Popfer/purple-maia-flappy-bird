d34376df3c0dff8f8500c6ae84c8c109faf2d17c
Popfer <32405025+Popfer@users.noreply.github.com>
2017-12-05 16:57:32 -1000

    added Main menu
    from video#2

--- assets/js/mainmenu.js
+++ assets/js/mainmenu.js
@@ -6,12 +6,19 @@ var mainMenuState = {
     
     
     preload: function() { 
-        
+        game.load.spritesheet('startBtn''assets/images/start_btn.png',193, 71);
+    
+         game.stage.backgroundColor = '#FFD700'
+         game.scale.pageAlighnHorizontally = true;    
+         game.scale.pageAlighnVertically = true;
     },
     
     
     create: function() {
+        startBtn = game.add.button(game.world.centerX-95, 200, 'startBtn', this.startGame, this, 2, 1, 0);
         
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+         spaceKey.onDown.add(this.startGame, this);
     },
     
     
